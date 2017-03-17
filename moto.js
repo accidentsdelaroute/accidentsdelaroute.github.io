c3198c400e596a815a78da89b982fa80a9fdf1ab
accidentsdelaroute <gabriele.ranieri@sciencespo.fr>
2017-03-17 13:09:22 +0100

    Update moto.js

--- moto.js
+++ moto.js
@@ -1,4 +1,9 @@
-Highcharts.chart('moto', {
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+
+<div id="container" style="min-width: 310px; max-width: 800px; height: 400px; margin: 0 auto">  </div>	
+
+Highcharts.chart('container', {
   chart: {
     type: 'column'
   },
