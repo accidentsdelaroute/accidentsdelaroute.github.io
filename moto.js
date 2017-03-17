5ecebb0f78d9bdbdbfe2108320f917329d6319a7
accidentsdelaroute <gabriele.ranieri@sciencespo.fr>
2017-03-17 12:32:03 +0100

    Update and rename casque.js to moto.js

---
+++ moto.js
@@ -0,0 +1,24 @@
+Highcharts.chart('container', {
+  chart: {
+    type: 'column'
+  },
+  title: {
+    text: 'Efficacité du casque selon les chocs'
+  },
+  xAxis: {
+    categories: ['Moto']
+  },
+  credits: {
+    enabled: false
+  },
+  series: [{
+    name: 'Choc frontal',
+    data: [-60]
+  }, {
+    name: 'Choc arrière',
+    data: [-83]
+  }, {
+    name: 'Autre choc',
+    data: [-80]
+  }]
+});
