efba917bcb843c0b64a6f8f3ca7db7f0c8deee6f
accidentsdelaroute <gabriele.ranieri@sciencespo.fr>
2017-03-17 12:33:27 +0100

    Create velo.js

---
+++ velo.js
@@ -0,0 +1,24 @@
+Highcharts.chart('container', {
+  chart: {
+    type: 'column'
+  },
+  title: {
+    text: 'Efficacité du casque selon les chocs'
+  },
+  xAxis: {
+    categories: ['Vélo']
+  },
+  credits: {
+    enabled: false
+  },
+  series: [{
+    name: 'Choc frontal',
+    data: [-3]
+  }, {
+    name: 'Choc arrière',
+    data: [-48]
+  }, {
+    name: 'Autre choc',
+    data: [-20]
+  }]
+});
