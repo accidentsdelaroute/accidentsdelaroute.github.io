25d591a6ac72b176b94ca82606413bf5963c0577
accidentsdelaroute <gabriele.ranieri@sciencespo.fr>
2017-03-17 12:34:29 +0100

    Create voiture.js

---
+++ voiture.js
@@ -0,0 +1,24 @@
+Highcharts.chart('container', {
+  chart: {
+    type: 'column'
+  },
+  title: {
+    text: 'Efficacité du casque selon les chocs'
+  },
+  xAxis: {
+    categories: ['Voiture']
+  },
+  credits: {
+    enabled: false
+  },
+  series: [{
+    name: 'Choc frontal',
+    data: [-91]
+  }, {
+    name: 'Choc arrière',
+    data: [-95]
+  }, {
+    name: 'Autre choc',
+    data: [-89]
+  }]
+});
