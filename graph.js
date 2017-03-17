b5d134f0ab9e066c479947d5d396c59ef4a3c9b1
accidentsdelaroute <gabriele.ranieri@sciencespo.fr>
2017-03-17 12:44:38 +0100

    Update graph.js

--- graph.js
+++ graph.js
@@ -7,7 +7,7 @@ Highcharts.chart('container', {
            
           },
     subtitle: { 
-      text: 'et leur évolution depuis 2005'
+      text: 'et leur évolution depuis '
     },
     xAxis: {
         categories: ['Paris', 'Bouches-du-Rhône','Val-de-Marne', 'Seine-Saint-Denis',  'Hauts-de-Seine', 'Haute-Corse', 'Alpes-Maritimes', 'Essonne', 'Indre-et-Loire', 'Rhône'],
